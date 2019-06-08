1e91e84f8f361f74afa514d11b208187154cbeac
Caio Lima <caio.pesquisa@hotmail.com>
2019-06-07 21:41:27 -0300

    Tela login

---
+++ Locadora/ClientApp/src/app/shared/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { Http } from '@angular/http';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  formulario: FormGroup;
+  categoryForm: FormGroup; 
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: Http
+  ) { }
+
+  ngOnInit() {
+  }
+
+
+  validarEmail() {
+    this.categoryForm = this.formBuilder.group({
+      email: [null, [Validators.required, Validators.minLength(2)]]
+    });
+  }
+
+}
