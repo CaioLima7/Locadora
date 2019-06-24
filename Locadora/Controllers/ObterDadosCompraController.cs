7fc2b855cc0758417ea09d5f9cba5ab6d3dbbcf1
Caio Lima <caio.pesquisa@hotmail.com>
2019-06-24 02:14:26 -0300

    Detalhes

---
+++ Locadora/Controllers/ObterDadosCompraController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Locadora.Banco.Entidades;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Locadora.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ObterDadosCompraController : ControllerBase
+    {
+        //[HttpPost]
+        //[Route("QuantidadeProduto")]
+        //public async Task<object> Teste(ItemPedido model)
+        //{
+        //    var buscar = await ItemPedidos.Where
+        //}
+    }
+}
